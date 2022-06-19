c5d75e1eb75f880a89684aa91feeae58f7aea147
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-06-19 20:21:17 -0300

    Update Agência_de_publicidade.py
    A mudança foi feita com a intenção de retirar variáveis desnecessárias do programa.

--- Agência_de_publicidade.py
+++ Agência_de_publicidade.py
@@ -3,32 +3,30 @@ v1=int(0)
 v2=int(0)
 v3=int(0)
 v4=int(0)
-v5=int(0)
-v6=int(0)
 while contador<7:
     anuncio = str(input(''))
-    if anuncio == 'R�dio':
+    if anuncio == 'Rádio':
         frequencia=str(input(''))
         if frequencia == 'AM':
             valor = int(300)
             v1 = valor + v1
         else:
             valor = int(500)
-            v2 = valor + v2
+            v1 = valor + v1
     elif anuncio == 'TV':
         horario = float(input(''))
         if horario > 20:
             valor = int(2000)
-            v3 = valor + v3
+            v2 = valor + v2
         else:
             valor = int(1200)
-            v4 = valor + v4
+            v2 = valor + v2
     elif anuncio == 'Revista':
         valor = int(750)
-        v5 = valor + v5
+        v3 = valor + v3
     else:
         valor = int(1500)
-        v6 = valor + v6
+        v4 = valor + v4
     contador +=1
-valor_final = (v1+v2+v3+v4+v5+v6)
+valor_final = (v1+v2+v3+v4)
 print('{:.2f}'.format(valor_final))
